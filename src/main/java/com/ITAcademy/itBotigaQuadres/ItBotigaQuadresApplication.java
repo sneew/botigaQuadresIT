c1179a9ec778bfe506a7a77618059325b60f787b
Eloi <eloi.femo12@gmail.com>
2020-05-25 13:32:34 +0200

    1st commit. import gradle project

---
+++ src/main/java/com/ITAcademy/itBotigaQuadres/ItBotigaQuadresApplication.java
@@ -0,0 +1,13 @@
+package com.ITAcademy.itBotigaQuadres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBotigaQuadresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItBotigaQuadresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ITAcademy/itBotigaQuadres/ItBotigaQuadresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITAcademy.itBotigaQuadres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItBotigaQuadresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
